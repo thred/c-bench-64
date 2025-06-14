9d0c2d1b1b9460fcfc911812ed289ffb6001bb38
Manfred Hantschel <thred@users.noreply.github.com>
2025-06-14 21:05:28 +0200

    Revamp makefiles

--- benchmarks/src/fact.c
+++ benchmarks/src/fact.c
@@ -5,7 +5,7 @@
 #define __reentrant
 #endif
 
-#define N_ITER 1000
+#define N_ITER 100
 
 #define SIZE 16
 
@@ -13,54 +13,58 @@ long array[SIZE];
 
 long fact(long i) __reentrant
 {
-  if(i>1) return i*fact(i-1);
-  else return 1;
+    if (i > 1)
+        return i * fact(i - 1);
+    else
+        return 1;
 }
 
 long res;
 
 void benchmark(void)
 {
-  int i,j;
+    int i, j;
 
-  res=0;
+    res = 0;
 
-  for(j=0;j<SIZE;j++) {
-    array[j]=0;
-  }
-
-
-  for(i=0;i<N_ITER;i++) {
-    for(j=0;j<SIZE;j++) {
-      array[j]+=fact(j);
+    for (j = 0; j < SIZE; j++)
+    {
+        array[j] = 0;
     }
-  }
 
-  for(j=0;j<SIZE;j++) {
-    res+=array[j];
-  }
+    for (i = 0; i < N_ITER; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
+            array[j] += fact(j);
+        }
+    }
 
+    for (j = 0; j < SIZE; j++)
+    {
+        res += array[j];
+    }
 }
 
 void benchmark_name(void)
 {
-  printf("fact.c\n");
-  printf("Calculates factorials (%d iterations)\n",N_ITER);
+    printf("fact.c\n");
+    printf("Calculates factorials (%d iterations)\n", N_ITER);
 }
 
 unsigned char benchmark_check(void)
 {
 
-  printf("res=%ld ",res);
-/*
-  if(prime_count==EXPECTED) {
+    printf("res=%ld ", res);
+    /*
+      if(prime_count==EXPECTED) {
+        printf("OK\n");
+        return 0;
+      } else {
+        printf("FAIL - expected %d\n",EXPECTED);
+        return 1;
+      }
+    */
     printf("OK\n");
     return 0;
-  } else {
-    printf("FAIL - expected %d\n",EXPECTED);
-    return 1;
-  }
-*/
- printf("OK\n");
- return 0;
 }
